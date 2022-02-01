4fe85bbabe271d6fceaaea78ce6478ee207a4c7e
Roberto Coral <33666303+robertocoral@users.noreply.github.com>
2022-02-01 13:17:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-var map = L.map('map').setView([38.639005110722664, -90.2845213858883], 12);
+var map = L.map('map').setView([37.76423006921716, -122.42960396594103], 11);
 
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
+var Stamen_Terrain = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
- $.getJSON("https://raw.githubusercontent.com/robertocoral/GIS5091-assignment2-part1/main/restaurants.geojson",function(data){
-    L.geoJSON(data, {
-  	onEachFeature: function (feature, layer) {
-    		layer.bindPopup('<h3>'+feature.properties.Restaurant+'</h3>');
-  		}
-	}).addTo(map);
- });
+
+$.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
+    var crimeIcon = L.icon({
+      iconUrl: 'https://www.s4g.com/thumbnail.asp?file=assets/images/mighty-mouse.jpg&maxx=400&maxy=0',
+      iconSize: [50,50]
+    });
+    var crime = L.geoJson(data,{
+      pointToLayer: function(feature,latlng){
+        var marker = L.marker(latlng,{icon: crimeIcon});
+        marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.OPEN_DT + '<br/>' + feature.properties.SUBJECT);
+        return marker;
+      }
+    });
+    var clusters = L.markerClusterGroup();
+    clusters.addLayer(crime);
+    map.addLayer(clusters);
+});
